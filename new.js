d1da7cf88ec3f84ad51370310f94c48615ac0647
asp <asp@Aptechnk1.edu>
2025-10-22 11:32:42 +0500

    my project

---
+++ new.js
@@ -0,0 +1,9 @@
+// Optional: Scroll to top button, or smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href'))?.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Cambridge Hospital Website Loaded");
+});
+
+
